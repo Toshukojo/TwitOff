1ebaf795a7957af7a0ccb46de009f5d10eed8905
Toshukojo <Toshukojo@users.noreply.github.com>
2020-08-13 23:12:07 -0400

    almost complete, added new files.

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,8 +1,10 @@
 """Code for our app"""
 
 from decouple import config
-from flask import Flask, render_template
-from TWITOFF.models import DB, User
+from flask import Flask, render_template, request
+from .models import DB, User
+from .predict import predict_user
+from .twitter import add_or_update_user
 
 
 # make our app factory
@@ -23,10 +25,37 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
 
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(user1_name=user1, user2_name=user2, tweet_text=request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
+
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base.html', title = 'Reset', users=[])
+        return render_template('base.html', title='Reset', users=[])
 
     return app
